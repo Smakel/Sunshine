afe9fe3118d371f81c005e825f94a8f83e1fe042
Smakel <schneicw@gmail.com>
2015-01-30 10:53:43 -0600

    Initial commit

---
+++ app/src/main/java/com/schneider/cody/sunshine/MainActivity.java
@@ -0,0 +1,80 @@
+package com.schneider.cody.sunshine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            String[] forecastarray = {
+                    "Today-Sunny-88/63",
+                    "Tomorrow-Cloudy--52/105",
+                    "Saturday-Acid Rain-23/98",
+                    "Sunday-Aliens-34/57",
+                    "Monday-Sunny-32/45",
+                    "Tuesday-Snow-34/56",
+                    "Wednesday-Snow-29/34"
+            };
+            List<String> weekForecast = new ArrayList<String>(
+                    Arrays.asList(forecastarray));
+
+            return rootView;
+        }
+    }
+}
